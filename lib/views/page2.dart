25c3943208142a7bc2e9ebe26c5084d7c9a64766
Rahma Khaled Zakaria <khaledrahma656@gmail.com>
2023-08-14 20:58:30 +0200

    cubit task

--- lib/views/page2.dart
+++ lib/views/page2.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:train1/views/cubit/todo_cubit.dart';
+// ignore: unused_import
 import 'package:train1/views/service/user_service.dart';
 
+// ignore: unused_import
 import 'model/usermodel.dart';
 
 class Page2 extends StatefulWidget {
@@ -11,32 +15,41 @@ class Page2 extends StatefulWidget {
 }
 
 class _Page2State extends State<Page2> {
-  List<UserModel> users = [];
-  bool isloaing = true;
-  getMyUsers() async {
-    users = await UserService().getUsers();
-    isloaing = false;
-    setState(() {});
-  }
-
   @override
-  void initState() {
-    super.initState();
-    getMyUsers();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return isloaing ? 
-    const Center(child: CircularProgressIndicator(),)
-    : ListView.builder(
-        itemCount: users.length,
+    return BlocProvider(
+      create: (context) => TodoCubit(),
+      child: BlocConsumer<TodoCubit, TodoState>(
+        builder: (context, state) {
+          if (state is TodoLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is TodoError) {
+            return const Center(
+              child: Text("error"),
+            );
+          }
+          return ListView.builder(
+        itemCount: context.watch<TodoCubit>().users.length,
         itemBuilder: (BuildContext context, int index) {
           return ListTile(
-            title: Text(users[index].title ?? "--"),
+            title: Text(
+                      context.watch<TodoCubit>().users[index].title ?? "--"),
             trailing: const Icon(Icons.person),
             leading: Text("${index + 1}"),
           );
         });
+
+
+        },
+        listener: (context, state) {},
+      ),
+    );
   }
 }
+
+
+
